3e9a012731d6449e53136b71e5576f3598c02bbb
umityatarkalkmaz <umityatarkalkmaz@gmail.com>
2024-10-12 23:31:43 +0300

    composer update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'umityatarkalkmaz/route',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'ba9b8bb637cd740068a9d081d765289e76794211',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'umityatarkalkmaz/route' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'ba9b8bb637cd740068a9d081d765289e76794211',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
